91224217ea1a6702ded194bf23494e530bd3493e
vesper-arch <vesper-arch@proton.me>
2025-03-27 18:03:33 -0400

    Make struct fields public

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,30 @@
-struct ClayColor(u8, u8, u8);
+pub mod clay_main {
+    pub struct ClayColor(u8, u8, u8);
 
-struct ClaySizingModeFixed {
-    x: i32,
-    y: i32,
-}
+    pub struct ClaySizingModeFixed {
+        pub x: i32,
+        pub y: i32,
+    }
 
-struct ClayBorderRadius(u32, u32, u32, u32);
+    pub struct ClayBorderRadius(u32, u32, u32, u32);
 
-enum ClayChildLayoutDirection {
-    LeftToRight,
-    TopToBottom,
-}
+    pub enum ClayChildLayoutDirection {
+        LeftToRight,
+        TopToBottom,
+    }
 
-struct ClayObject {
-    sizing: ClaySizingModeFixed,
-    color: ClayColor,
-    layout_direction: ClayChildLayoutDirection,
-    border_radius: ClayBorderRadius,
-    padding: (u32, u32, u32, u32)
+    pub struct ClayObject {
+        pub sizing: ClaySizingModeFixed,
+        pub color: ClayColor,
+        pub layout_direction: ClayChildLayoutDirection,
+        pub border_radius: ClayBorderRadius,
+        pub padding: (u32, u32, u32, u32)
+    }
 }
 
 pub mod clay_raylib {
     use raylib::prelude::*;
-    use super::*;
+    use crate::clay_main;
 
     pub fn raylib_init() -> (RaylibHandle, RaylibThread){
         let (rl, thread) = raylib::init()
@@ -33,7 +35,7 @@ pub mod clay_raylib {
         return (rl, thread)
     }
 
-    pub fn draw_object(test_obj: ClayObject, mut draw_handle: RaylibDrawHandle) {
+    pub fn draw_object(test_obj: clay_main::ClayObject, mut draw_handle: RaylibDrawHandle) {
         draw_handle.draw_rectangle(5, 5, test_obj.sizing.x, test_obj.sizing.y, Color::BLUEVIOLET);
     }
 }
