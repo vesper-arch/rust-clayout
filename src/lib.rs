8ae1168ee531d89eefb96a7dccd14cb17c913597
vesper-arch <vesper-arch@proton.me>
2025-03-27 17:20:28 -0400

    Add actual layout stuff, have to find out how to test this

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,45 @@
-use raylib::prelude::*;
+struct ClayColor(u8, u8, u8);
 
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+struct ClaySizingModeFixed {
+    x: i32,
+    y: i32,
 }
 
-pub fn clay_raylib_initialize() -> (RaylibHandle, RaylibThread) {
-    let (rl, thread) = raylib::init()
-        .size(640, 480)
-        .title("Stupid rectangle die piece of shit")
-        .vsync()
-        .build();
-    return (rl, thread)
+struct ClayBorderRadius(u32, u32, u32, u32);
+
+enum ClayChildLayoutDirection {
+    LeftToRight,
+    TopToBottom,
+}
+
+struct ClayObject {
+    sizing: ClaySizingModeFixed,
+    color: ClayColor,
+    layout_direction: ClayChildLayoutDirection,
+    border_radius: ClayBorderRadius,
+    padding: (u32, u32, u32, u32)
+}
+
+mod clay_raylib {
+    use raylib::prelude::*;
+    use super::*;
+
+    pub fn raylib_init() {
+        let (mut rl, thread) = raylib::init()
+            .size(640, 480)
+            .title("hell")
+            .build();
+
+        while !rl.window_should_close() {
+            let mut d = rl.begin_drawing(&thread);
+             
+            d.clear_background(Color::WHITE);
+        }
+    }
+
+    pub fn draw_object(test_obj: ClayObject, mut draw_handle: RaylibDrawHandle) {
+        draw_handle.draw_rectangle(5, 5, test_obj.sizing.x, test_obj.sizing.y, Color::BLUEVIOLET);
+    }
 }
 
 #[cfg(test)]
@@ -19,7 +48,6 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+        assert_eq!(2 + 4, 6);
     }
 }
